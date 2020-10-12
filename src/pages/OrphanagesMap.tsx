dfce19e300fd51780e10188ca2227b6361f702f7
Guilherme Oliveira Casagrande <guiolicasa@gmail.com>
2020-10-12 13:33:34 -0300

    Colocando o mapa na página de orfanatos

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
-function OrphanagesMap(){
+import mapMarkerImg from "../images/map-marker.svg"
+import { FiPlus } from "react-icons/fi"
+
+import { Map, TileLayer } from "react-leaflet"
+
+import "leaflet/dist/leaflet.css"
+
+import "../styles/pages/orphanages-map.css"
+
+function OrphanagesMap() {
     return (
-        <h1>Mapa dos orfanato</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy" />
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>São José dos Campos</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+            <Map
+                center={[-23.1891287, -46.0031]}
+                zoom={15}
+                style={{ width: "100%", height: "100%" }}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            </Map>
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
     );
 }
 
