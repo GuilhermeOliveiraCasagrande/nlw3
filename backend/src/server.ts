289f4e75da0a405602975857f9f8ce4e817b8ee6
Guilherme Oliveira Casagrande <guiolicasa@gmail.com>
2020-10-13 11:58:39 -0300

    Instalando o db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,23 @@
 import express from "express"
 
+/* Métodos HTTP
+    GET -> buscando info
+    POST -> criando novo info
+    PUT -> alterando info
+    DELETE -> removendo info
+*/
+
+/* Parâmetros
+    Query params: localhost:3000/users?filter=active
+    Route params: localhost:3000/users/1
+    Body params: vão no body da requisição
+*/
+
 const app = express()
 app
-    .get("/", (req, res) => {
-        res.send("<h1>Bom dia!</h1>")
+    .use(express.json())
+    .get("/users/:id", (req, res) => {
+        
     }).listen(3000, () => {
         console.log("Server ligado na porta 3000")
     })
