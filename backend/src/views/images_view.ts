3372f5f6a4efc4e8076acf489d9ce53b47ec04d8
Guilherme Oliveira Casagrande <guiolicasa@gmail.com>
2020-10-15 09:36:12 -0300

    Atualizando image_views para arrimar o bug de não aparecer a imagem

--- backend/src/views/images_view.ts
+++ backend/src/views/images_view.ts
@@ -5,7 +5,7 @@ export default {
     render(image: Image) {
         return {
             id: image.id,
-            url: `https://localhost:3000/uploads/${image.path}` /* Retorna a url para baixar a imagem */
+            url: `http://localhost:3000/uploads/${image.path}` /* Retorna a url para baixar a imagem */
         }
     },
     renderMany(images: Image[]) {
