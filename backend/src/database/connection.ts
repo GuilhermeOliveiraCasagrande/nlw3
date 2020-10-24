2f7461932da48a74a200b9769a9cd488f87dbc8c
Guilherme Oliveira Casagrande <guiolicasa@gmail.com>
2020-10-24 14:36:22 -0300

    Fixing connection.ts not finding db entities

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,5 +1,16 @@
 //Conecta com o db
-
 import { createConnection } from "typeorm"
 
-createConnection();
+createConnection({
+    "type":"sqlite",
+    "database":"./src/database/database.sqlite",
+    "migrations":[
+        "./src/database/migrations/*.ts"
+    ],
+    "cli":{
+        "migrationsDir":"./src/database/migrations/"
+    },
+    "entities":[
+        "./src/models/*.ts"
+    ]
+});
